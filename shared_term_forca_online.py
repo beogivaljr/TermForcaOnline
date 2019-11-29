1b65e612ea8ac22d695bf10013fd6647895cd384
beogivaljr <beogivaljr@gmail.com>
2019-11-29 00:31:02 -0300

    resolves #10

--- shared_term_forca_online.py
+++ shared_term_forca_online.py
@@ -7,14 +7,35 @@ PORT = 8080  # Port to listen on (non-privileged ports are > 1023)
 __ENCODING = 'utf-8'  # '__' Para representar que é para uso interno
 
 # Comandos API
+
+# Tudo o que vier depois dessa tag será interpretado como entrada do usuário e não pode ser lido como comando
 API_USER_INPUT = 'USER_INPUT '
+
+# Usada para requisitar dados
 API_GET = 'GET '
+
+# Usada para enviar dados
 API_POST = 'POST '
+
+# Usada para indicar o primeiro contato com o servidor
 API_TOUCH = 'TOUCH '
+
+# Usada para marcar a comunicação com o primeiro jogador
 API_FIRST = 'FIRST '
-API_SUCCESS = ''
-API_ERROR_500 = 'HTTP/1.1 500 ERROR\r\n\r\n'
-API_END = 'HTTP/1.1\r\n\r\n'
+
+# Mensagem padrão de sucesso
+API_SUCCESS = 'HTTP/1.1 200 OK\r\n'
+
+# Mensagem padrão de erro
+API_ERROR_500 = 'HTTP/1.1 500 ERROR\r\n'
+
+# Final padrão do cabeçalho
+API_END = 'HTTP/1.1\r\n'
+
+# Exemplos de requisições
+# 'POST TOUCH HTTP/1.1\r\n'
+# 'POST USER_INPUT HTTP/1.1\r\nQualquer texto inserido pelo usuário'
+# 'FIRST HTTP/1.1 200 OK\r\n'
 
 
 # Recebe uma string
