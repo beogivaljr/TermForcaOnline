4d1ca6cd8af54e1b457a8e8c1fb075690da952f7
beogivaljr <beogivaljr@gmail.com>
2019-11-30 17:15:30 -0300

    resolves #6

--- servidor_descubra_palavra.py
+++ servidor_descubra_palavra.py
@@ -6,8 +6,9 @@ import threading
 import time
 import socket
 
-# Mensagens
+# Mensagens para o servidor
 TITLE_MAIN = 'Main:'
+TITLE_MAIN_GAME = 'Main Game:'
 TITLE_FIRST_PLAYER = 'Primeiro jogador:'
 TITLE_GUESSING_PLAYER = 'Jogador'
 MSG_WAITING_NEW_PLAYERS = 'Esperando novos jogadores...'
@@ -15,11 +16,23 @@ MSG_CONNECTED = 'Conectado'
 MSG_TOTAL_CONNECTED_PLAYERS = 'Total de jogadores ->'
 MSG_DISCONNECTED = 'Desconectado.'
 MSG_SERVER_CLOSED = 'Servidor encerrado.'
+MSG_GAME_STARTED = 'Jogo iniciado'
 
-# Variáveis globais
+# Mensagens para o cliente
+CLT_MSG_TOO_LONG_WORD = 'Palavra muito longa, por favor escolha uma palavra mais curta.'
+
+# Constantes do servidor
+TOTAL_GAME_TIME = 9  # Tempo total de jogo em segundos
+STOP_RECEIVING = 'STOP_RECEIVING '
+
+# Variáveis globais do servidor
 connected_players = []  # Lista com os jogadores ativos
 main_thread_id = 0
 first_player_thread_id = 0
+main_game_thread_id = 0
+game_word = None
+current_game_time = TOTAL_GAME_TIME
+game_status_needs_update = False
 
 
 # Recebe o objeto usuário da connexão do socket {player = (connection, address)}
@@ -55,8 +68,13 @@ def log(any_object):
     current_thread_alias = current_thread_id % 1000
     if current_thread_id is main_thread_id:
         logging.info(f'{TITLE_MAIN} {any_object}')
+
+    elif current_thread_id is main_game_thread_id:
+        logging.info(f'{TITLE_MAIN_GAME} {any_object}')
+
     elif current_thread_id is first_player_thread_id:
         logging.info(f'{TITLE_FIRST_PLAYER} {any_object}')
+
     else:
         logging.info(f'{TITLE_GUESSING_PLAYER} {current_thread_alias}: {any_object}')
 
@@ -75,11 +93,12 @@ def log_total_players():
 def disconnect(player):
     if player in connected_players:
         connected_players.remove(player)
-    get_player_connection(player).close()
-    log(MSG_DISCONNECTED)
-    log_total_players()
+        get_player_connection(player).close()
+        log(f'{get_player_ip(player)} {MSG_DISCONNECTED}')
+        log_total_players()
 
 
+# TODO: Fluxo dos demais jogadores (jogadores adivinhadores)
 # Thread paralela que trata os usuários que vão tentar adivinhar a palavra proposta pelo primeiro jogador
 # Temos uma thread dessas por usuário
 def treat_guessing(player):
@@ -87,7 +106,7 @@ def treat_guessing(player):
     with get_player_connection(player) as conn:
         try:
             print('TODO GUEST')
-            time.sleep(5)
+            time.sleep(20)
         finally:
             disconnect(player)
 
@@ -96,31 +115,54 @@ def treat_guessing(player):
 # Traduz os comandos recebidos pelo cliente através da requisição
 # Devolve a resposta adequada para ser enviada ao cliente
 def translate_first_players(request):
-    if API_USER_INPUT in request:  # Deve ignorar qualquer outro tipo de comando case tenha entrada do usuário
-        print(request)
-        return request
-    elif API_POST in request:
-        if API_TOUCH in request:
-            return f'{API_POST}{API_FIRST}{API_END}'  # Comando para avisar o cliente que este jogador foi o primeiro
-        elif API_GET in request:
-            return API_GET
-    return 'response'
+    if f'{API_POST}{API_TOUCH}' in request:
+        return f'{API_POST}{API_FIRST}{API_TOUCH}'
+
+    elif f'{API_POST}{API_USER_INPUT}' in request:
+        chosen_word = get_content_from(request)  # Nova string contendo a entrada do usuário
+        if len(chosen_word) <= MAX_INPUT_LENGTH:
+            main_game_tread = threading.Thread(target=setup_game_with, args=(chosen_word,))
+            main_game_tread.start()
+            log(MSG_GAME_STARTED)
+            return API_SUCCESS
+        else:
+            return f'{API_POST}{API_FIRST}{API_USER_ERROR}{CLT_MSG_TOO_LONG_WORD}'
+    elif f'{API_POST}{API_START}' in request:
+        run_game_timer()
+        return STOP_RECEIVING
+
+    else:
+        return f'{API_POST}{API_FIRST}{API_BAD_REQUEST}'
 
 
 # Thread paralela que trata o primeiro usuário, responsável por digitar a palavra que vão tentar adivinhar
 # Temos uma thread dessas só para o primeiro usuário
-def treat_first(player):
+def handle_first(player):
     global first_player_thread_id
     first_player_thread_id = threading.current_thread().ident  # Captura o id da thread do primeiro jogador
     log(MSG_CONNECTED)
     with get_player_connection(player) as conn:
         try:
             while True:
-                r = conn.recv(1024)  # Aguarda a receber a requisição
+                request = decode(conn.recv(MAX_PACK_LENGTH))  # Aguarda a receber a requisição
 
                 # Após a resposta bem sucedida
-                response = translate_first_players(decode(r))  # Traduz a requisição e gera a resposta
-                conn.sendall(encode(response))  # Envia a resposta
+                if API_END in request:  # Checa se a requisição tem fim no cabeçalho
+                    response = translate_first_players(request)  # Traduz a requisição e gera uma resposta
+                    if STOP_RECEIVING in response:
+                        break
+                    else:
+                        conn.sendall(encode(response))  # Envia a resposta ao cliente
+                else:
+                    # Envia 400 e encerra a conexão
+                    conn.sendall(encode(API_BAD_REQUEST))
+                    disconnect(player)
+                    break
+
+            # Aguarda o jogador encerrar a conexão ou ignora jogadores já desconectados
+            while conn.fileno() is not -1:
+                pass
+
         except BrokenPipeError as e:
             log(e)
         except ConnectionResetError as e:
@@ -130,6 +172,73 @@ def treat_first(player):
             conn.sendall(encode(API_ERROR_500))
         finally:
             disconnect(player)
+
+
+# Envia o 'status_description' do jogo para todas as conexões ativas
+def send_all_players(game_status):
+    for player in connected_players:
+        try:
+            get_player_connection(player).sendall(encode(game_status))  # Envia status do jogo
+        finally:  # Ignora quaisquer erros
+            pass
+
+
+# TODO: Função que monta uma str com o do status do jogo
+# IMPORTANTE - Só pode ser chamada por uma thread pois será fonte de conflitos
+# Função responsável por montar e enviar a descrição do status do jogo
+def update_game_status():
+    status_description = 'Status do jogo'
+    send_all_players(status_description)
+
+
+# TODO: Função que cuida do jogo corrente
+# Thread responsável pelo jogo corrente
+def setup_game_with(word):
+    global main_game_thread_id
+    main_game_thread_id = threading.current_thread().ident
+    global game_word
+    game_word = word
+    global game_status_needs_update
+    game_status_needs_update = True
+
+    # Loop principal do jogo
+    while current_game_time > 0:
+        # Verifica se uma atualização é necessária
+        if game_status_needs_update:
+            update_game_status()
+            game_status_needs_update = False
+            time.sleep(0.5)  # Tempo mínimo até a próxima atualização
+
+    # Após o fim do jogo
+    finish_game()
+
+
+# Subtrai um de current_timer até que chegue no zero
+def run_game_timer():
+    global current_game_time
+    global game_status_needs_update
+    if current_game_time > 0:
+        current_game_time -= 1
+        threading.Timer(1, function=run_game_timer).start()
+
+        # Pede atualização do status a cada dois segundos
+        if current_game_time % 2 == 0:
+            game_status_needs_update = True
+
+
+# TODO: Função que finaliza o jogo
+# Finaliza o jogo
+# Gera e envia o ultimo status do jogo, contendo ganhadores e aviso de fim de jogo
+# Disconecta todos os jogadores
+def finish_game():
+    last_status_description = 'Fim de jogo'
+    send_all_players(last_status_description)
+
+    global current_game_time
+    current_game_time = TOTAL_GAME_TIME  # Reseta o timer
+    # Disconecta todos os jogadores
+    while len(connected_players) > 0:
+        disconnect(connected_players[0])
 
 
 # Execussão principal do programa
@@ -146,7 +255,7 @@ if __name__ == '__main__':
                 # Após conexão bem sucedida
                 connected_players.append(new_player)
                 if total_connected_players() == 1:
-                    t = threading.Thread(target=treat_first, args=(new_player,))
+                    t = threading.Thread(target=handle_first, args=(new_player,))
                 else:
                     t = threading.Thread(target=treat_guessing, args=(new_player,))
                 t.start()
